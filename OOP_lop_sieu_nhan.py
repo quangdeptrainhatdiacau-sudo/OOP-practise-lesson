50daf4e7f3a72771ed4da4d0e62d3b71e49403ac
NGUYỄN MINH QUANG <quangdeptrainhatdiacau@gmail.com>
2026-03-19 13:07:35 +0700

    Implement SieuNhan class with initialization and string method

---
+++ OOP_lop_sieu_nhan.py
@@ -0,0 +1,17 @@
+class SieuNhan:
+    ten = str
+    vu_khi = str
+    mau_sac = str
+
+    def __init__(self, ten="", vu_khi="", mau_sac=""):
+        self.ten = ten
+        self.vu_khi = vu_khi
+        self.mau_sac = mau_sac
+    
+    def __str__(self):
+        return"Siêu nhân %s, Vũ khí %s, Màu %s" % (self.ten, self.vu_khi, self.mau_sac)
+
+sieunhanA = SieuNhan("A", "Kiếm", "Xanh")
+sieunhanB = SieuNhan("B", "Khiên", "Đỏ")
+
+print(sieunhanA,"\n",sieunhanB)
